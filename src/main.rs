6d96f634b142622dc0d7b72f82c776d6bbb78265
Sean Hellum <seanhellum45@gmail.com>
2020-01-20 23:56:01 +0000

    implement backspace

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use crossterm::event::{read, Event, KeyCode};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::io::Read;
 use std::io::Write;
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::process::exit;
 use structopt::StructOpt;
@@ -9,11 +11,22 @@ struct Cli {
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
+pub fn lines_from_file<T: AsRef<Path>>(filename: T) -> String {
+    let file = std::fs::File::open(&filename);
+    let file = match file {
+        Ok(n) => n,
+        Err(_) => {
+            println!("Error! File not found!");
+            exit(0);
+        }
+    };
+    return std::fs::read_to_string(&filename).unwrap();
+}
 fn get_file<T: AsRef<Path>>(filename: T) -> std::fs::File {
     let file = std::fs::OpenOptions::new()
-                                    .write(true)
-                                    .create(true)
-                                    .open(filename);
+        .write(true)
+        .create(true)
+        .open(filename);
     let file = match file {
         Ok(n) => n,
         Err(_) => {
@@ -26,19 +39,22 @@ fn get_file<T: AsRef<Path>>(filename: T) -> std::fs::File {
 #[allow(unused_must_use)]
 fn main() -> std::io::Result<()> {
     let args = Cli::from_args();
-    let mut file = get_file(args.path);
+    let mut file = get_file(&args.path);
     enable_raw_mode().unwrap();
     loop {
         let event = read().unwrap();
         if event == Event::Key(KeyCode::Esc.into()) {
             break;
+        } else if event == Event::Key(KeyCode::Backspace.into()) {
+            let contents = lines_from_file(&args.path);
+            std::fs::write(&args.path, &contents[0..contents.len() - 1]);
         } else {
             for i in b' '..b'~' {
                 if event == Event::Key(KeyCode::Char(i as char).into()) {
-                    file.write(&[i]);
+                    &file.write(&[i]);
                 }
             }
-        } 
+        }
     }
     disable_raw_mode().unwrap();
     Ok(())
