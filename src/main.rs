19704a08068a2e4ec17ee96b2354728aa7be24aa
Sean Hellum <seanhellum45@gmail.com>
2020-01-20 22:36:43 +0000

    add q

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ struct Cli {
     path: std::path::PathBuf,
 }
 fn get_file<T: AsRef<Path>>(filename: T) -> std::fs::File {
-    let file = std::fs::File::open(filename);
+    let file = std::fs::OpenOptions::new()
+                                    .write(true)
+                                    .create(true)
+                                    .open(filename);
     let file = match file {
         Ok(n) => n,
         Err(_) => {
@@ -27,12 +30,11 @@ fn main() -> std::io::Result<()> {
     enable_raw_mode().unwrap();
     loop {
         let event = read().unwrap();
-        println!("Event: {:?}\r", event);
         if event == Event::Key(KeyCode::Esc.into()) {
             break;
         } else {
             if event == Event::Key(KeyCode::Char('q').into()) {
-                file.write(b"q").unwrap();
+                file.write(b"q");
             }
         } 
     }
